2a55e37a6d362d044dff340e73ffdfc52f79ddde
hebert <heberte.2004@gmail.com>
2025-05-16 22:03:32 -0300

    aula 06:agenda

--- 06/ContatoDAO.php
+++ 06/ContatoDAO.php
@@ -25,13 +25,20 @@ class ContatoDAO
         return $contatos;
     }
 
-    public function getById(int $id): ?contato{
+    public function getById(int $id): ?Contato
+    {
         $stmt = $this->db->prepare("SELECT * FROM agenda.contatos WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        return $row? new Contato($row['id'], $row['nome'], $row['telefone'], $row['email'], $row['endereco']): null;
+        return $row? new Contato(
+            $row['id'], 
+            $row['nome'], 
+            $row['telefone'], 
+            $row['email'], 
+            $row['endereco'])
+            : null;
     }
 
     public function create(Contato $contato) 
@@ -49,6 +56,13 @@ class ContatoDAO
         $stmt->bindParam(':telefone', $telefone);
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':endereco', $endereco);
+        $stmt->execute();
+    }
+
+    public function delete(int $id): void
+    {
+        $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+        $stmt->bindParam(':id', $id);
         $stmt->execute();
     }
 }
