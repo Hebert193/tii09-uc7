a714913d47f6d3e1383141948bddac0edff67a74
hebert <heberte.2004@gmail.com>
2025-06-27 20:20:40 -0300

    aula 12: projeto

---
+++ 12/produtos/listar.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../dao/ProdutoDAO.php';
+$dao = new ProdutoDAO();
+$produtos = $dao->getAll();
+?>
+<h1>Produtos</h1>
+<?php foreach ($produtos as $p): ?>
+    <p>
+        <a href="ver.php?id=<?= $p->getId() ?>">
+            <?= htmlspecialchars($p->getNome()) ?> - R$ <?= number_format($p->getPreco(), 2, ',', '.') ?>
+        </a>
+        <br>
+        <small>Fornecedor:Nome</small>
+    </p>
+
+<?php endforeach; ?>
+<br>
+<a href="../index.php">Voltar</a>
