70268b19a871411954ee0990e6a909ff668a1d17
hebert <heberte.2004@gmail.com>
2025-06-27 22:15:10 -0300

    aula 12: proj

--- 12/produtos/listar.php
+++ 12/produtos/listar.php
@@ -1,7 +1,10 @@
 <?php
 require_once __DIR__ . '/../dao/ProdutoDAO.php';
+require_once __DIR__ . '/../core/authService.php';
 $dao = new ProdutoDAO();
 $produtos = $dao->getAll();
+
+$user = getLoggedUser();
 ?>
 <h1>Produtos</h1>
 <?php foreach ($produtos as $p): ?>
@@ -10,7 +13,12 @@ $produtos = $dao->getAll();
             <?= htmlspecialchars($p->getNome()) ?> - R$ <?= number_format($p->getPreco(), 2, ',', '.') ?>
         </a>
         <br>
-        <small>Fornecedor:Nome</small>
+        <small>Fornecedor: <?= $p->getFornecedor() ? $p->getFornecedor()->getNome() : 'Não tem' ?></small>
+
+        <?php if($user): ?>
+            | <a href="./editar.php?id=<?= $p->getId() ?>">Editar</a>
+            | <a href="./excluir.php?id=<?= $p->getId() ?>" onclick="return confirm('Tem certeza?')">Excluir</a>
+        <?php endif; ?>
     </p>
 
 <?php endforeach; ?>
